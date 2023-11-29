f2b81669956fd6e27ebb5867f9ca3227c2857bfc
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2023-11-29 09:53:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -59,113 +59,83 @@ st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/377104762625954
 
 # Conteúdo da página "MyxoNet"
 with tab2:
-    st.header("MyxoNet")
-    
-    from yolo_predictions import YOLO_Pred
-    from PIL import Image
-    import numpy as np
+   import streamlit as st
+from yolo_predictions import YOLO_Pred
+from PIL import Image
+import numpy as np
 
-    st.write('Por favor, carregue a imagem para obter a identificação')
+st.set_page_config(page_title="YOLO Object Detection",
+                   layout='wide',
+                   page_icon='./images/object.png')
 
-    with st.spinner('Por favor, aguarde enquanto analisamos a sua imagem'):
-        yolo = YOLO_Pred(onnx_model='./best.onnx',
-                         data_yaml='./data.yaml')
-        #st.balloons()
+st.header('Get Object Detection for any Image')
+st.write('Please Upload Image to get detections')
 
-    def upload_image():
-        # Upload Image
-        image_file = st.file_uploader(label='Enviar Imagem')
-        if image_file is not None:
-            size_mb = image_file.size / (1024 ** 2)
-            file_details = {"filename": image_file.name,
-                            "filetype": image_file.type,
-                            "filesize": "{:,.2f} MB".format(size_mb)}
-            #st.json(file_details)
-            # validate file
-            if file_details['filetype'] in ('image/png', 'image/jpeg'):
-                st.success('Tipo de arquivo imagem VALIDO (png ou jpeg)')
-                return {"file": image_file,
-                        "details": file_details}
+with st.spinner('Please wait while your model is loading'):
+    yolo = YOLO_Pred(onnx_model='./models/best.onnx',
+                    data_yaml='./models/data.yaml')
+    #st.balloons()
 
-            else:
-                st.error('Tipo de arquivo de imagem INVALIDO')
-                st.error('Upload only png, jpg, jpeg')
-                return None
-
-    def main():
-        object = upload_image()
-
-        if object:
-            prediction = False
-            image_obj = Image.open(object['file'])
-
-            col1, col2 = st.columns(2)
-
-            with col1:
-                st.info('Pré-visualização da imagem')
-                st.image(image_obj)
-
-            with col2:
-                st.subheader('Confira abaixo os detalhes do arquivo')
-                st.json(object['details'])
-                button = st.button('Descubra qual o Myxozoário pode estar presente em sua imagem')
-                if button:
-                    with st.spinner("""
-                    Obtendo Objets de imagem. Aguarde
-                    """):
-                        # below command will convert
-                        # obj to array
-                        image_array = np.array(image_obj)
-                        pred_img = yolo.predictions(image_array)
-                        pred_img_obj = Image.fromarray(pred_img)
-                        prediction = True
-
-            if prediction:
-                st.subheader("Imagem com a possivel detecção")
-                st.caption("Detecção de Myxozoários")
-                st.image(pred_img_obj)
-
-    if __name__ == "__main__":
-         main()
-
-def display_detection_results(predictions):
-    st.subheader("Detecções na imagem")
-    for pred in predictions:
-        st.write(f"Objeto: {pred['label']}, Confiança: {pred['confidence']}")
-        st.image(pred['image'], caption='Detecção', use_column_width=True)
-
+def upload_image():
+    # Upload Image
+    image_file = st.file_uploader(label='Upload Image')
+    if image_file is not None:
+        size_mb = image_file.size/(1024**2)
+        file_details = {"filename":image_file.name,
+                        "filetype":image_file.type,
+                        "filesize": "{:,.2f} MB".format(size_mb)}
+        #st.json(file_details)
+        # validate file
+        if file_details['filetype'] in ('image/png','image/jpeg'):
+            st.success('VALID IMAGE file type (png or jpeg')
+            return {"file":image_file,
+                    "details":file_details}
+        
+        else:
+            st.error('INVALID Image file type')
+            st.error('Upload only png,jpg, jpeg')
+            return None
+        
 def main():
     object = upload_image()
-
+    
     if object:
         prediction = False
-        image_obj = Image.open(object['file'])
-
-        col1, col2 = st.columns(2)
-
+        image_obj = Image.open(object['file'])       
+        
+        col1 , col2 = st.columns(2)
+        
         with col1:
-            st.info('Pré-visualização da imagem')
+            st.info('Preview of Image')
             st.image(image_obj)
-
+            
         with col2:
-            st.subheader('Confira abaixo os detalhes do arquivo')
+            st.subheader('Check below for file details')
             st.json(object['details'])
-            button = st.button('Descubra qual o Myxozoário pode estar presente em sua imagem')
+            button = st.button('Get Detection from YOLO')
             if button:
-                with st.spinner("Obtendo Objetos de imagem. Aguarde"):
+                with st.spinner("""
+                Geting Objets from image. please wait
+                                """):
+                    # below command will convert
+                    # obj to array
                     image_array = np.array(image_obj)
                     pred_img = yolo.predictions(image_array)
+                    pred_img_obj = Image.fromarray(pred_img)
                     prediction = True
-
+                
         if prediction:
-            display_detection_results(pred_img)
-
+            st.subheader("Predicted Image")
+            st.caption("Object detection from YOLO V5 model")
+            st.image(pred_img_obj)
+    
+    
+    
 if __name__ == "__main__":
     main()
 
 
-
-# Conteúdo da página "MYXOscopeAPP"
+# Conteúdo da página "USB"
 with tab3:
     st.header("USB")
 
