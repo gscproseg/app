677635c69fb35bc7ab83bd7af48034f40e0eecb9
Gustavo Souza Carneiro <gs.gustavocarneiro@gmail.com>
2023-11-30 14:34:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -63,23 +63,26 @@ with tab1:
 # Adicione as informações adicionais
 st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/3771047626259544)")
 
+pass
+
 
 #######################################################
 
-from yolo_predictions import YOLO_Pred
-from PIL import Image
-import numpy as np
+with tab1:
+    from yolo_predictions import YOLO_Pred
+    from PIL import Image
+    import numpy as np
 
 
-st.header('Get Object Detection for any Image')
-st.write('Please Upload Image to get detections')
+    st.header('Get Object Detection for any Image')
+    st.write('Please Upload Image to get detections')
 
-with st.spinner('Please wait while your model is loading'):
+    with st.spinner('Please wait while your model is loading'):
     yolo = YOLO_Pred(onnx_model='./best.onnx',
                     data_yaml='./data.yaml')
     #st.balloons()
 
-def upload_image():
+    def upload_image():
     # Upload Image
     image_file = st.file_uploader(label='Upload Image')
     if image_file is not None:
@@ -99,51 +102,48 @@ def upload_image():
             st.error('Upload only png,jpg, jpeg')
             return None
         
-def main():
-    object = upload_image()
+    def main():
+        object = upload_image()
     
-    if object:
-        prediction = False
-        image_obj = Image.open(object['file'])       
+        if object:
+            prediction = False
+            image_obj = Image.open(object['file'])       
         
-        col1 , col2 = st.columns(2)
+            col1 , col2 = st.columns(2)
         
-        with col1:
-            st.info('Preview of Image')
-            st.image(image_obj)
+            with col1:
+                st.info('Preview of Image')
+                st.image(image_obj)
             
-        with col2:
-            st.subheader('Check below for file details')
-            st.json(object['details'])
-            button = st.button('Get Detection from YOLO')
-            if button:
-                with st.spinner("""
-                Geting Objets from image. please wait
+            with col2:
+                st.subheader('Check below for file details')
+                st.json(object['details'])
+                button = st.button('Get Detection from YOLO')
+                if button:
+                    with st.spinner("""
+                    Geting Objets from image. please wait
                                 """):
-                    # below command will convert
-                    # obj to array
-                    image_array = np.array(image_obj)
-                    pred_img = yolo.predictions(image_array)
-                    pred_img_obj = Image.fromarray(pred_img)
-                    prediction = True
+                        # below command will convert
+                        # obj to array
+                        image_array = np.array(image_obj)
+                        pred_img = yolo.predictions(image_array)
+                        pred_img_obj = Image.fromarray(pred_img)
+                        prediction = True
                 
-        if prediction:
-            st.subheader("Predicted Image")
-            st.caption("Object detection from YOLO V5 model")
-            st.image(pred_img_obj)
+            if prediction:
+                st.subheader("Predicted Image")
+                st.caption("Object detection from YOLO V5 model")
+                st.image(pred_img_obj)
     
     
     
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
 
-# Adicione as informações adicionais
-st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/3771047626259544)")
+    # Adicione as informações adicionais
+    st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/3771047626259544)")
 
-
-# Adicione as informações adicionais
-st.write("Desenvolvido por [Carneiro, G.S](http://lattes.cnpq.br/3771047626259544)")
-
+#####################################################################################
 
 # Conteúdo da página "MYXOscopeAPP"
 with tab3:
