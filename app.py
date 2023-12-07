465dec7f817dcc3a2cbe9918c8eeb384b18487bf
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2023-12-06 21:32:54 -0300

    app.py
    🧠

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Configuração da página
 st.set_page_config(
     page_title= "MyxoNet",
-    page_icon= "🔬",  # Defina o ícone da página como um emoji de tubarão
+    page_icon= "🧠",  # Defina o ícone da página como um emoji de tubarão
     layout="wide",  # Defina o layout como "wide" para aproveitar melhor o espaço na tela
     initial_sidebar_state="collapsed"  # Defina a barra lateral como colapsada
 )
